8104d2c51241c11590bb792db29f463e834fb414
anatolik2509 <antonov250901@mail.ru>
2020-07-20 02:03:35 +0300

    requests

--- app/routes/users_routes.js
+++ app/routes/users_routes.js
@@ -1,6 +1,8 @@
+bodyParser = require("body-parser").json();
+
 module.exports = function (app) {
     app.get('/profile', (request, response) => {
-        var result = {
+        let result = {
             "first_name":"Antonov",
             "last_name":"Anatoly",
             "age":18,
@@ -10,6 +12,54 @@ module.exports = function (app) {
             "skills":["bababa", "bebebe"],
             "info":"Учусь в итисе. Сейчас сижу, вёрстку изучаю."
         }
+        response.setHeader("Content-Type", "application/json");
         response.send(JSON.stringify(result));
+    });
+
+    app.post('/users', bodyParser, (request, response) => {
+        let body = request.body;
+        let responseBody = {
+            id : Math.random(),
+            "name" : body["name"]
+        }
+
+        response.setHeader("Content-Type", "application/json");
+        response.send(JSON.stringify(result));
+    });
+
+    app.post('/resume', bodyParser, (request, response) => {
+        let body = request.body;
+        const {Client} = require('pg');
+        const client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            password: 'itis2020',
+            database: 'mySiteDB'
+        });
+        const req = 'INSERT INTO resumes(name, phone, text) VALUES($1, $2, $3) RETURNING *';
+        client.connect();
+        client.query(req,
+            [body['name'], body['phone'], body['text']],
+            (err, data) => {
+                response.setHeader("Content-Type", "application/json");
+                response.send(JSON.stringify(data.rows));
+                client.end();
+            })
+    });
+
+    app.get('/resume', (request, response) => {
+        const { Client } = require('pg');
+        const client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            password: 'itis2020',
+            database: 'mySiteDB'
+        });
+        client.connect();
+        client.query('SELECT name, phone, text FROM resumes', (err, data) => {
+            response.setHeader("Content-Type", "application/json");
+            response.send(JSON.stringify(data.rows));
+            client.end();
+        });
     });
 };
